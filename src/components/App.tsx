1d9257f86c380bbe05ae911cf3d88cb54969fb48
sarath <sarathsairaju@gmail.com>
2021-09-22 22:04:07 +0800

    feat(app): Made initial changes to create a react app []

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface AppProps { }
+
+export default function App(props: AppProps) {
+    // npx eslint src/components/* --fix
+    return <h1>Hello React!</h1>;
+}
